b2c439c6d1f2481e5cbbffec809cb538d5bee4f5
unknown <qebcarampot@tip.edu.ph>
2023-01-23 23:34:23 +0800

    tailwind change

--- Gram-Coffee/tailwind.config.cjs
+++ Gram-Coffee/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -8,26 +8,26 @@ module.exports = {
         "gray-50": "#EFE6E6",
         "gray-100": "#DFCCCC",
         "gray-500": "#5E0000",
-        "primary-100": "#FFE1E0",
-        "primary-300": "#FFA6A3",
-        "primary-500": "#FF6B66",
+        "primary-100": "#e5d3b3",
+        "primary-300": "#b99976",
+        "primary-500": "#664229",
         "secondary-400": "#FFCD5B",
-        "secondary-500": "#FFC132",
+        "secondary-500": "#b99976",
       },
       backgroundImage: (theme) => ({
         "gradient-yellowred":
           "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
-          // "mobile-home": "url('./assets/HomePageGraphic.png')",
+        "mobile-home": "url('./assets/HomePageGraphic.png')",
       }),
       fontFamily: {
         dmsans: ["DM Sans", "sans-serif"],
         montserrat: ["Montserrat", "sans-serif"],
       },
       content: {
-        // evolvetext: "url('./assets/EvolveText.png')",
-        // abstractwaves: "url('./assets/AbstractWaves.png')",
-        // sparkles: "url('./assets/Sparkles.png')",
-        // circles: "url('./assets/Circles.png')",
+        evolvetext: "url('./assets/hand.png')",
+        abstractwaves: "url('./assets/AbstractWaves.png')",
+        sparkles: "url('./assets/Sparkles.png')",
+        circles: "url('./assets/Circles.png')",
       },
     },
     screens: {
